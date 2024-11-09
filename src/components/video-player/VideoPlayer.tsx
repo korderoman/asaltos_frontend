e61fcd5e306118bf266ba86f4a80d0d1e107ea84
HenryMedina <hmedinar@proveedor.intercorp.com.pe>
2024-11-09 00:01:07 -0500

    estilo de los videos

--- src/components/video-player/VideoPlayer.tsx
+++ src/components/video-player/VideoPlayer.tsx
@@ -7,7 +7,7 @@ export interface VideoPlayerInterface {
 const VideoPlayer= (props:VideoPlayerInterface):JSX.Element =>{
     return (
         <div>
-            <video className={"w-100"} controls>
+            <video className={"w-100 video-fixed-height"} controls>
                 <source src={props.url} type="video/mp4" />
                 Tu navegador no soporta la reproducción de videos.
             </video>
