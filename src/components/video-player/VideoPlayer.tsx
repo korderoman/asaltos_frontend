a8c0653f745b2d7dc6e35ae2264fe95ac3dec331
HenryMedina <hmedinar@proveedor.intercorp.com.pe>
2024-11-08 22:55:48 -0500

    mp4

--- src/components/video-player/VideoPlayer.tsx
+++ src/components/video-player/VideoPlayer.tsx
@@ -8,7 +8,7 @@ const VideoPlayer= (props:VideoPlayerInterface):JSX.Element =>{
     return (
         <div>
             <video width="640" height="360" controls>
-                <source src={props.url} type="video/avi" />
+                <source src={props.url} type="video/mp4" />
                 Tu navegador no soporta la reproducción de videos.
             </video>
         </div>
